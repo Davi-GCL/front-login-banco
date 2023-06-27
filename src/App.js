18c8462e6cd5ff21e196e5823aaf9f996f2764d8
davi.lemos <1davi.gabriel1@gmail.com>
2023-06-27 13:29:22 -0300

    axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import bootstrap from 'bootstrap';
+import {useEffect, useState} from "react";
 import './App.css';
+import api from "./services/api";
 
 import CardCadastro from './comps/CardCadastro';
 
 function App() {
+
+  const [teste, setTeste] = useState();
+
+  // useEffect(()=>{
+  //   api.get("Usuario").then((res)=>console.log(res.data))
+  //   .catch((err)=>{console.error("Ocorreu um erro!" + err)})
+  // },[]);
+
+
   return (
     <div className="container w-100">
       <div className='row justify-content-center'>
+        
         <CardCadastro/>
       </div>
     </div>
--- src/comps/CardCadastro.js
+++ src/comps/CardCadastro.js
@@ -1,6 +1,17 @@
-
+import { useEffect } from "react"
+import api from "../services/api";
 
 export default function CardCadastro(){
+
+  function handleSubmit(e){
+    e.preventDefault()
+
+    var formData = e.target;
+
+    console.log(formData.get("field1"));
+
+    // api.post("Usuario/Create", formData).then((res)=>{console.log(res.data)}).catch(err=>console.error(err));
+  }
 
 
     return(
@@ -8,22 +19,23 @@ export default function CardCadastro(){
           <div className="card-title">
             <h4 className='text-center'>Cadastre-se gratuitamente!</h4>
           </div>
-          <form action="" method="post" className='card-body'>
+
+          <form className='card-body' id="form-cadastro" onSubmit={(e)=>handleSubmit(e)}>
             <div className="row mb-4">
-              <label htmlFor="input-name" className="form-label h6">Nome:</label>
-              <input type="text" name='input-name' className="form-control" />
+              <label htmlFor="nome" className="form-label h6">Nome:</label>
+              <input type="text" name='nome' className="form-control" />
             </div>
             <div className="row mb-4">
-              <label htmlFor="input-conta" className="form-label h6">Nº Conta:</label>
-              <input type="text" name='input-conta' className="form-control" />
+              <label htmlFor="conta" className="form-label h6">Nº Conta:</label>
+              <input type="text" name='conta' className="form-control" />
             </div>
             <div className="row mb-4">
-              <label htmlFor="input-senha" className="form-label h6">Senha:</label>
-              <input type="password" name='input-senha' className="form-control" />
+              <label htmlFor="senha" className="form-label h6">Senha:</label>
+              <input type="password" name='senha' className="form-control" />
             </div>
             <div className="row mb-4">
-              <label htmlFor="input-confsenha" className="form-label h6">Confirmar Senha:</label>
-              <input type="password" name='input-confsenha' className="form-control" />
+              <label htmlFor="confsenha" className="form-label h6">Confirmar Senha:</label>
+              <input type="password" name='confsenha' className="form-control" />
             </div>
             <div className="button-area row">
               <button type="submit" className='btn btn-success w-100'>Cadastrar</button>
