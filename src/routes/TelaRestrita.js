5cdee7750037deb680d1d5e3dfc1e8f510112e8f
Davi-GCL <1davi.gabriel1@gmail.com>
2023-07-24 10:26:00 -0300

    useEffect no get contas

--- src/routes/TelaRestrita.js
+++ src/routes/TelaRestrita.js
@@ -1,39 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function TelaRestrita() {
   const id = localStorage.getItem('loginId');
-  const [conta,setConta] = useState({
-    codConta: '',
+  const [contas,setContas] = useState([{
+    codConta: 0,
     saldo: 0,
     tipo: 0
-  })
+  }])
   
 
-  fetch(`https://localhost:7044/Conta/`, {
-    method: 'GET',
-  })
-  .then((response) => response.json())
-  .then((data) => {
+  async function handleGet(){
+    let res = await fetch(`https://localhost:7044/Conta/`, {
+      method: 'GET',
+    })
+
+    let data = await res.json()
     console.log(data);
     data.forEach(e => {
       if(e.idUsuario == id){
-        setConta({['codConta']:e.codConta, ['saldo']:e.saldo});
-      }
-    });
-  })
-  .catch(err => console.log(err));
+        setContas([...contas,{['codConta']:e.codConta, ['saldo']:e.saldo , ['tipo']:e.tipo}]);
+      }  
+    })
+  }
 
-  
+  //Algoritmo para que faz a função de buscar dados na api só seja executada uma vez:
+  useEffect(() => {
+    handleGet();
+  }, []); // A lista de dependências vazia indica que o efeito será executado apenas uma vez, após a montagem do componente.
 
   return (
-    <div className='alert alert-light'>
-        <h5>Conta N°{conta['codConta']}</h5>
-        <hr></hr>
-      <ul>
-        <li>Saldo: R${conta['saldo']}</li>
-        <li>Tipo: {conta['tipo'] == 0? 'Poupança':'Corrente'}</li>
-      </ul>
-    </div>
+    <React.Fragment>
+      {contas.map((conta)=>{
+        if(conta.codConta !== 0){
+          return (<div className='alert alert-light' key={conta.codConta}>
+            <h5>Conta N°{conta['codConta']}</h5>
+            <hr></hr>
+          <ul>
+            <li className='password'>Saldo: R${conta['saldo']}</li>
+            <li>Tipo: {conta['tipo'] == 0? 'Poupança':'Corrente'}</li>
+          </ul>
+        </div>)
+        }else{ return null }
+      })
+      }
+    </React.Fragment>
   )
 }
 
